98e2c39b85f23430973c708c8f8666fd2be9dc17
arr213 <arr213@gmail.com>
2017-01-06 12:44:20 -0500

    fixed render

--- background/index.js
+++ background/index.js
@@ -1,3 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppContainer from './react/containers/AppContainer';
+
+ReactDOM.render(
+  <AppContainer />,
+  document.getElementById('app')
+);
+
 var config = {
   apiKey: "AIzaSyBB_aazFI4lfQHjbV5aT8Sijn7g_QFWVus",
   authDomain: "tabby-birthday.firebaseapp.com",
---
+++ background/react/containers/AppContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class AppContainer extends Component {
+
+  render () {
+    return (
+      <div>
+        <h2>This is our app.</h2>
+        <img src="http://lorempixel.com/400/200" />
+      </div>
+    )
+  }
+}
